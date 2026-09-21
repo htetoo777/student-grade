24b48293eb7059f19048bfe89b8b4474e2e1730e
uit885065-code <htetoo218@gmail.com>
2026-09-21 15:40:44 +0630

    1st

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input("Enter student's name: ")
+
+mark1 = float(input("Enter mark for subject 1: "))
+mark2 = float(input("Enter mark for subject 2: "))
+
+average = (mark1 + mark2) / 2
+
+print("Student Name:", name)
+print("Average Mark:", average)
